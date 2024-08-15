2403763e0ec63e9b79b70500c7aa8637a0d6bee7
ale-sha <81110628+ale-sha@users.noreply.github.com>
2024-08-15 18:26:07 +0300

    Add files via upload

---
+++ hw_2.py
@@ -0,0 +1,5 @@
+home_work_count = 12
+hour_count = 1.5
+course_name = 'Python'
+time_one_task = hour_count/float(home_work_count)
+print('Курс: ' + course_name,",всего задач: " + str(home_work_count),',затрачено часов: ' + str(hour_count),",среднее время выполнения: " + str(time_one_task))
