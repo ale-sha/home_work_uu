c0cabbdefff2ed37870a7a17cf33174e11bfc80e
ale-sha <81110628+ale-sha@users.noreply.github.com>
2024-08-20 10:48:33 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,13 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        add_matrix = []
+        matrix.append(add_matrix)
+        for j in range(m):
+           add_matrix.append(value)
+    return matrix
+
+result = get_matrix(3,4,5)
+print(result)
+
+
