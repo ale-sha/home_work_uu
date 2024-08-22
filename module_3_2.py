148950e9c462cb0a95ae97589241ab13457a259a
ale-sha <81110628+ale-sha@users.noreply.github.com>
2024-08-22 22:11:19 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,27 +1,16 @@
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    simbol = [".com", ".ru", ".net"]
-    count = 0
+    simbol = (".com", ".ru", ".net")
     if recipient == sender:
         print("Нельзя отправить письмо самому себе!")
+        return
+    if not (recipient.__contains__('@') and sender.__contains__('@')
+            and any(map(recipient.endswith, simbol)) and any(map(sender.endswith, simbol))):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+    if sender != 'university.help@gmail.com':
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
     else:
-        if recipient.__contains__('@') and sender.__contains__('@'):
-            for i in range(len(simbol)):
-                if count < 2:
-                    if recipient.__contains__(simbol[i]) == sender.__contains__(simbol[i]) == True:
-                        count += 1
-                    elif recipient.__contains__(simbol[i]) or sender.__contains__(simbol[i]):
-                        count += 1
-                    else:
-                        continue
-            if count == 2:
-                if sender != 'university.help@gmail.com':
-                    print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
-                else:
-                    print(f"Письмо успешно отправлено c адреса {sender} на адрес {recipient}")
-            else:
-                print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-        else:
-            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        print(f"Письмо успешно отправлено c адреса {sender} на адрес {recipient}")
 
 
 send_email('jnjnj', 'hpm@ail.ru')
