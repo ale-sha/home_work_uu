b895c5939b8f353b3c4ced312660ba9ec9b0a333
ale-sha <81110628+ale-sha@users.noreply.github.com>
2024-08-28 15:21:50 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as divide_fake
+from true_math import divide as divide_true
+
+print(divide_fake(33, 11))
+print(divide_fake(3, 0))
+print(divide_true(12, 2))
+print(divide_true(5, 0))
