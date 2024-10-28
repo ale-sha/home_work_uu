9c0cc8952887f0d74e0b212e9ea568658006a232
ale-sha <81110628+ale-sha@users.noreply.github.com>
2024-10-28 22:45:59 +0300

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,43 @@
+'''Lambda-функция'''
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+
+
+'''Замыкание'''
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for set_data in data_set:
+                file.write(str(set_data) + '\n')
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+'''Метод __call__'''
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        from random import choice
+        for word in self.words:
+            word = choice(self.words)
+            return word
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
