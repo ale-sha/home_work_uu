98b066cc272077dc3ed6b93225b180c05bd055e2
Alena Shabalina <81110628+ale-sha@users.noreply.github.com>
2024-12-17 10:45:52 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filemode='w',
+    filename='runner_tests.log',
+    encoding='UTF-8',
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
